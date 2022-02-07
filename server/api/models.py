d005ce0c94d0452ad546c8a089039484c3ce8d82
Lefan <lefantan@hotmail.com>
2022-02-06 22:30:07 -0700

    reset

--- server/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Author(models.Model):
-    displayName = models.CharField(max_length=100)
-    admin = models.BooleanField(default=False)
--- social_distribution/asgi.py
+++ social_distribution/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for server project.
+ASGI config for social_distribution project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_distribution.settings')
 
 application = get_asgi_application()
