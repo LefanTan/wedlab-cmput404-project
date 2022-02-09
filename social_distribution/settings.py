b5615a8358a05e7e723b19bde9366348a4899561
Sandip Saha Joy <sandipsahajoy@gmail.com>
2022-02-09 03:08:45 -0700

    update

--- social_distribution/settings.py
+++ social_distribution/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -82,7 +85,7 @@ DATABASES = {
         ssl_require=True
         )
 }
-
+print('DATABASE [Current] :', DATABASES['default'])
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
