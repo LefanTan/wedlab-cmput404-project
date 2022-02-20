aa0ccf973817fa42869683965eb07d91fb88522c
Lefan <lefantan@hotmail.com>
2022-02-19 23:43:17 -0700

    basic homepage header

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from service.models import Author
+from django.forms.models import model_to_dict
 
 
 def home(request):
-    return render(request, 'home.html')
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    try:
+        author = Author.objects.get(user=request.user.id)
+    except Exception as e:
+        pass
+    return render(request, 'home.html', model_to_dict(author))
