09fa5de3b35c16753d9ccbec2d4bd8e873bdddc7
Lefan <lefantan@hotmail.com>
2022-02-22 18:20:44 -0700

    added a tag input

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('post/create', post_form, name='post_create'),
+    path('post/edit', post_form, name='post_edit')
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import redirect, render
-
+from django.views.defaults import page_not_found
 from service.models import Author
 from django.forms.models import model_to_dict
 
 
-def home(request):
+def auth_check_middleware(request):
     if not request.user.is_authenticated:
-        return redirect('login')
+        return (redirect('login'), False)
 
     try:
         author = Author.objects.get(user=request.user.id)
     except Exception as e:
-        pass
-    return render(request, 'home.html', model_to_dict(author))
+        return (page_not_found(request, e), False)
+    return (author, True)
+
+
+def home(request):
+    author, success = auth_check_middleware(request)
+
+    if success:
+        return render(request, 'home.html', model_to_dict(author))
+    return author
+
+
+def post_form(request):
+    author, success = auth_check_middleware(request)
+
+    if success:
+        return render(request, 'post_form.html', context={"author": model_to_dict(author), "name": request.resolver_match.url_name})
+    return author
