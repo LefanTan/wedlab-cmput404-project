52d0f3323a3dd1a951fa256725be37574a3952d3
Lefan <lefantan@hotmail.com>
2022-02-24 16:11:41 -0700

    try without

--- service/tests.py
+++ service/tests.py
@@ -13,16 +13,12 @@ class AuthEndpointsTestCase(APITestCase):
         self.factory = APIRequestFactory()
         self.apiClient = APIClient()
 
-    @override_settings(DEBUG=True)
     def test_create_user(self):
         response = self.apiClient.post(reverse('signup'), data=urllib.parse.urlencode({
             "username": "admin",
             "password": "root",
             "github": "https://link.com"
         }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
-
-        print(response.content)
-        print(settings.DEBUG)
 
         success = self.apiClient.login(username="admin", password="root")
 
