92760e10eebaafdcda2db93a669055acdd9d1d63
Lefan <lefantan@hotmail.com>
2022-02-24 18:20:57 -0700

    basic tests

--- service/tests.py
+++ service/tests.py
@@ -1,18 +1,24 @@
-from django.test import TestCase, override_settings
+from rest_framework import status
 from django.urls import reverse
-from django.conf import settings
 from .models import Author
-import os
+import json
 import urllib
 from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory, APIClient, APITestCase
 
 
-class AuthEndpointsTestCase(APITestCase):
+def get_dict(byte):
+    return json.loads(byte.decode("UTF-8"))
+
+
+class AuthorEndpointsTestCase(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.apiClient = APIClient()
+        self.default_user = User.objects.create_user(
+            username="default_user", password="root")
 
+    # Test signup endpoint and login
     def test_create_user(self):
         response = self.apiClient.post(reverse('signup'), data=urllib.parse.urlencode({
             "username": "admin",
@@ -20,9 +26,77 @@ class AuthEndpointsTestCase(APITestCase):
             "github": "https://link.com"
         }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
 
-        print(os.environ.get('CI'))
-
         success = self.apiClient.login(username="admin", password="root")
 
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+        try:
+            author = Author.objects.get(displayName="admin")
+        except Exception as e:
+            self.fail('Unable to retrieve author object')
+
         self.assertTrue(success)
+
+    def test_get_authors(self):
+        # User 1
+        self.apiClient.post(reverse('signup'), data=urllib.parse.urlencode({
+            "username": "user1",
+            "password": "root",
+            "github": "https://link.com"
+        }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
+        # User 2
+        self.apiClient.post(reverse('signup'), data=urllib.parse.urlencode({
+            "username": "user2",
+            "password": "root",
+            "github": "https://link.com"
+        }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
+        # User 3
+        self.apiClient.post(reverse('signup'), data=urllib.parse.urlencode({
+            "username": "user3",
+            "password": "root",
+            "github": "https://link.com"
+        }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
+
+        response = self.apiClient.get(reverse(
+            'author_list'), SERVER_NAME="test.com")
+
+        dict = get_dict(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(3, len(dict.get('items')))
+
+
+class PostEndpointsTestCase(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.apiClient = APIClient()
+
+        self.apiClient.post(reverse('signup'), data=urllib.parse.urlencode({
+            "username": "admin",
+            "password": "root",
+            "github": "https://link.com"
+        }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
+        self.apiClient.login(username="admin", password="root")
+
+        self.author = Author.objects.get(displayName="admin")
+
+    def test_create_post_with_list(self):
+        # Create two post
+        self.apiClient.post(reverse('post_list', kwargs={"author_pk": self.author.id}), data=urllib.parse.urlencode({
+            "title": "title1",
+            "description": "description text",
+        }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
+        self.apiClient.post(reverse('post_list', kwargs={"author_pk": self.author.id}), data=urllib.parse.urlencode({
+            "title": "title2",
+            "description": "description text",
+        }), SERVER_NAME="test.com", content_type="application/x-www-form-urlencoded")
+
+        # query the post by author id
+        response = self.apiClient.get(reverse('post_list', kwargs={
+            "author_pk": self.author.id}), SERVER_NAME="test.com")
+
+        dict = get_dict(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual("title1", dict[0].get('title'))
+        self.assertEqual("title2", dict[1].get('title'))
--- service/urls.py
+++ service/urls.py
@@ -5,12 +5,13 @@ urlpatterns = [
     path('auth/signup/', author_views.signup, name='signup'),
     path('service/', include([
         # Author Endpoints
-        path('authors/', author_views.author_list),
-        path('authors/<str:pk>', author_views.author_detail),
+        path('authors/', author_views.author_list, name='author_list'),
+        path('authors/<str:pk>', author_views.author_detail, name='author_detail'),
 
         # Post Endpoints
         path('authors/<str:author_pk>/posts/<str:post_pk>',
-             post_views.post_detail),
-        path('authors/<str:author_pk>/posts', post_views.posts)
+             post_views.post_detail, name='post_detail'),
+        path('authors/<str:author_pk>/posts',
+             post_views.posts, name='post_list')
     ]))
 ]
