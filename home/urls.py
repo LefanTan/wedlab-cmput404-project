6319359c89d9d5b18a2ee76f6eb4e0643e0de4f9
alisha03 <crasta@ualberta.ca>
2022-03-01 02:36:08 -0700

    share posts frontend

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('authors/<str:author_pk>', profile, name='profile'),
     path('authors/<str:author_pk>/posts', post_list, name='home_post_list'),
     path('post/create', post_create, name='home_post_create'),
-    path('post/<str:post_pk>/edit', post_edit, name='home_post_edit')
+    path('post/<str:post_pk>/edit', post_edit, name='home_post_edit'),
+    path('post/<str:post_pk>/share', share_post, name='share_post')
 ]
