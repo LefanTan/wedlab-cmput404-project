9866c5ee613408b9ebe8fc9ced5460bf5126fdca
Sandip Saha Joy <sandipsahajoy@gmail.com>
2022-03-07 03:05:41 -0700

    comment fully works

--- service/serializers.py
+++ service/serializers.py
@@ -69,56 +69,8 @@ class PostSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
-
-        # TODO: Temporary comment data!
-        # ret['commentsSrc'] = {
-        #     "comments": [
-        #         {
-        #             "type": "comment",
-        #             "author": {
-        #                 "type": "author",
-        #                 # ID of the Author (UUID)
-        #                 "id": "http://127.0.0.1:8000/authors/1d698d25ff008f7538453c120f581471",
-        #                 # url to the authors information
-        #                 "url": "http://127.0.0.1:8000/authors/1d698d25ff008f7538453c120f581471",
-        #                 "host": "http://127.0.0.1:8000/",
-        #                 "displayName": "Greg Johnson",
-        #                 # HATEOS url for Github API
-        #                 "github": "http://github.com/gjohnson",
-        #                 # Image from a public domain
-        #                 "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
-        #             },
-        #             "comment": "**Comment** with markdown!",
-        #             "contentType": "text/markdown",
-        #             # ISO 8601 TIMESTAMP
-        #             "published": "2015-03-09T13:07:04+00:00",
-        #             # ID of the Comment (UUID)
-        #             "id": "http://127.0.0.1:8000/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
-        #         },
-        #         {
-        #             "type": "comment",
-        #             "author": {
-        #                 "type": "author",
-        #                 # ID of the Author (UUID)
-        #                 "id": "http://127.0.0.1:8000/authors/4cee67d276f048559e27c7e800e388c3",
-        #                 # url to the authors information
-        #                 "url": "http://127.0.0.1:8000/authors/4cee67d276f048559e27c7e800e388c3",
-        #                 "host": "http://127.0.0.1:8000/",
-        #                 "displayName": "lefan2",
-        #                 # HATEOS url for Github API
-        #                 "github": "http://github.com/gjohnson",
-        #                 # Image from a public domain
-        #                 "profileImage": ""
-        #             },
-        #             "comment": "Sick Olde English",
-        #             "contentType": "text/plain",
-        #             # ISO 8601 TIMESTAMP
-        #             "published": "2015-03-09T13:07:04+00:00",
-        #             # ID of the Comment (UUID)
-        #             "id": "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
-        #         }
-        #     ]
-        # }
+        comments = Comment.objects.filter(post=instance)
+        ret["comments"] = CommentSerializer(comments, many=True).data
         return ret
 
 
