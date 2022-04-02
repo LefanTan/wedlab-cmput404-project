1b74a493d06bef783ce2983151763bb992edc0ca
Lewis <36083021+lewisning@users.noreply.github.com>
2022-04-02 14:21:44 -0600

    UPDATE: Follow/Unfollow button on the follower detail page

--- home/urls.py
+++ home/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('post/<str:post_pk>/edit', post_edit, name='home_post_edit'),
     path('post/<str:post_pk>/share', share_post, name='share_post'),
     path('inbox', inbox, name='inbox'),
-    path('followers/<str:foreign_author_pk>', follower_details, name='follower_details')
+    path('followers/<str:foreign_author_pk>', follower_detail, name='follower_details')
 ]
--- home/views.py
+++ home/views.py
@@ -132,18 +132,31 @@ def followers(request):
     return author
 
 
-def follower_details(request, foreign_author_pk):
+def follower_detail(request, foreign_author_pk):
     author, success = auth_check_middleware(request)
+    isFollowed = False
 
     try:
         request_author = Author.objects.get(pk=foreign_author_pk)
+
     except Exception as e:
         return page_not_found(request, e)
+
+    try:
+        forward = FollowRequest.objects.get(actor=author, object=foreign_author_pk)
+        if forward is not None:
+            isFollowed = True
+        else:
+            isFollowed = False
+    except Exception:
+        pass
 
     if success:
         if request.method == 'GET':
             return render(request, 'follower_detail.html',
-                          context={"author": model_to_dict(request_author), "name": request.resolver_match.url_name})
+                          context={"author": model_to_dict(request_author), "name": request.resolver_match.url_name,
+                                   "isFollowed": isFollowed, "edit": author.id == foreign_author_pk,
+                                   "error": request.GET.get('error')})
     return author
 
 
@@ -234,24 +247,3 @@ def share_post(request, post_pk):
                                    "allauthors": allAuthors})
     return author
 
-
-def follower_detail(request, author_pk):
-    # Show author's profile
-    author, success = auth_check_middleware(request)
-    isFollowed = False
-
-    if FollowRequest.objects.get(actor=author.id, object=author_pk) is not None:
-        isFollowed = True
-
-    try:
-        request_author = Author.objects.get(pk=author_pk)
-    except Exception as e:
-        return page_not_found(request, e)
-
-    if success:
-        if request.method == 'GET':
-            return render(request, 'follower_detail.html',
-                          context={"author": model_to_dict(request_author), "name": request.resolver_match.url_name,
-                                   "isFollowed": isFollowed, "edit": author.id == author_pk,
-                                   "error": request.GET.get('error')})
-    return author
