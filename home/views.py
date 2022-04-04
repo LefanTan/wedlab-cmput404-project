2785e3b9c2b5472a42917067c2e5b3079ab4e26a
Lewis <36083021+lewisning@users.noreply.github.com>
2022-04-04 11:04:15 -0600

    FINISHED: Comments related Likes show up in inbox

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ import base64
 from django.shortcuts import redirect, render
 from django.core.paginator import Paginator
 from django.views.defaults import page_not_found
-from service.models import Author, Host, Post, FollowRequest, InboxObject, Comment, LikePost
+from service.models import Author, Host, Post, FollowRequest, InboxObject, Comment, LikePost, LikeComment
 from service.serializers import PostSerializer, FollowRequestSerializer
 from django.forms.models import model_to_dict
 from itertools import chain
@@ -200,6 +200,7 @@ def inbox(request):
     requests = None
     comments = None
     likes = None
+    likesComment = None
     followRequests = []
     posts = []
 
@@ -207,6 +208,7 @@ def inbox(request):
         allMessage = InboxObject.objects.all()
         comments = Comment.objects.all()
         likes = LikePost.objects.all()
+        likesComment = LikeComment.objects.all()
 
         # Store all messages into a list
         for i in allMessage:
@@ -231,7 +233,7 @@ def inbox(request):
             return render(request, 'inbox.html',
                           context={"author": model_to_dict(author), "messages": otherMessage, "requests": requests,
                                    "posts": posts, "comments": comments, "content": followRequests, "likes": likes,
-                                   "name": request.resolver_match.url_name})
+                                   "likeComment": likesComment ,"name": request.resolver_match.url_name})
     return author
 
 
